6a1be66cc84e971ef963bf60939d5a6d2f07b601
sbs863 <samtrey@gmail.com>
2017-07-20 12:25:40 -0500

    escaped an apostrophe

--- src/shared/component/page/home.jsx
+++ src/shared/component/page/home.jsx
@@ -41,7 +41,7 @@ const HomePage = () =>
               <img className="card-img-top" src="..." alt="Card" />
               <div className="card-block" >
                 <h4 className="card-title" >Card title</h4>
-                <p className="card-text" >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text" >Some quick example text to build on the card title and make up the bulk of the card&#39;s content.</p>
                 <button type="button" role="button" data-toggle="modal" data-target=".js-modal-example" className="btn btn-primary" >Open Modal</button>
               </div>
             </div>
